0b14cb29e01e0eddcb013f441c6d322137d2f304
nirajmgr1010 <neerazmgr9@gmail.com>
2025-11-19 09:40:25 +0545

    modify file

--- TODOLIST.java
+++ TODOLIST.java
@@ -1,11 +1,12 @@
 /*
  * 4	4. To-Do List: Use ArrayList to add, delete, and mark tasks complete.
  * 1. Add Task
-Add simple tasks
-Add task with priority (High/Medium/Low)
-Add task with deadline date
-Add task with category (Work, Study, Personal)
-Store them in an ArrayList
+Ask: Task Name?                                       
+Ask: Priority? (High/Med/Low)                         
+Ask: Deadline Date?                                   
+Ask: Category? (Work/Study/Personal)                  
+Create Task Object                                    
+Add to ArrayList                                      
 2. View All Tasks
 3. Delete a Task ->User enters task number → remove from ArrayList.
 5. Edit Task Name -> Example: “Finish homework” → “Finish homework (Math)”
@@ -21,45 +22,56 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.print.attribute.standard.MediaSize.NA;
 
 class Details implements Serializable{
-         private static String FullName;
-         private String TaskName;
-         Details(String name,String task){
-            FullName = name;
-             this.TaskName = task;
+         private String Taskname;
+         private String Priority;
+         private String Category;
+         private String DeadlineDay;
+         private LocalDateTime localDateTime;
+         Details(String t,String p,String c,String d){
+             this.Taskname = t;
+             this.Category = c;
+             this.Priority = p;
+             this.DeadlineDay = d;
+             this.localDateTime = LocalDateTime.now();
          }
-         public String GetName(){
-            return FullName;
-         }
-         public String GetTask(){
-            return TaskName;
-         }
+         // public String GetName(){
+         //    return FullName;
+         // }
+         // public String GetTask(){
+         //    return TaskName;
+         // }
          @Override
          public String toString() {
-         return "Fullname: "+FullName+"Task: "+TaskName;
+         return "TaskName: "+Taskname+", Priority: "+Priority+", Category: "+Category+", DeadlineDay: "+DeadlineDay+" "+localDateTime;
          }
-
 }
 class Task{
    ArrayList<Details> details;
-   private String FullName;
-   private String Task;
+   private String Taskname;
+   private String Priority;
+   private String Category;
+   private String DeadlineDay;
    private static final String fullPath = "TODOLIST.ser";
    Task(){
       details = new ArrayList<>();
        load();
    }
-   public void SetName(String Name,String task){
-      this.FullName = Name;
-      this.Task = task;
+   public void SetName(String task,String priority,String c,String d){
+       this.Taskname = task;
+       this.Category = c;
+       this.Priority = priority;
+       this.DeadlineDay = d;
       toAdd();
    }
    private void toAdd(){
-       details.add(new Details(FullName, Task));
+       details.add(new Details(Taskname,Priority,Category,DeadlineDay));
       Save();
    }
    private void Save(){
@@ -93,7 +105,49 @@ class Task{
 public class TODOLIST {
    public static void main(String[] args) {
     Task t = new Task();
-    t.SetName("Kulshingh","Homework");
-    t.SetName("bishal", "Do push up");
+    Scanner sc = new Scanner(System.in);
+   // System.out.println("Enter a how many students you want to enter details");
+   // int num = sc.nextInt();
+   //  while(num>0){
+   //  System.out.println("Enter Your Name: ");
+   //  String name = sc.nextLine();
+   //  sc.nextLine();
+   //  System.out.println("Enter your Task Name: ");
+   //  String taskNname = sc.nextLine();
+   //  t.SetName(name, taskNname);
+   //  num--;
+   //  }
+   System.out.println("Enter any number to choose choice ->");
+   System.out.println("1.Add a Task");
+   System.out.println("2. View Task");
+   System.out.println("3. Delete Task");
+   System.out.println("4. Mark Complete");
+   System.out.println("5. Edit Task");
+   System.out.println("6. Clear all Task");
+   System.out.println("7. Save to file");
+   System.out.println("8. Search task");
+   System.out.println("9. Exit....");
+   int num = sc.nextInt();
+   sc.nextLine();
+   switch (num) {
+      case 1:
+         System.out.println("Enter you task name: ");
+         String taskNname = sc.nextLine();
+         
+          System.out.println("Type task priority between high-medium-low type correct -> ");
+          String Priority = sc.nextLine();
+          System.out.println("Enter category ->");
+          String Category = sc.nextLine();
+          System.out.println("Enter deadline Date on Day");
+          String Day = sc.nextLine();
+            t.SetName(taskNname,Priority,Category,Day);
+         break;
+      
+         case 2:
+         t.Display();
+         break;
+      default:
+         break;
+   }
    } 
 }
