190090265d1c8b3c2a1ae8d120ce5a8529866389
nirajmgr1010 <neerazmgr9@gmail.com>
2025-11-23 17:56:08 +0545

    modify LibraryManagementSystem

--- Librarymanagementsystem.java
+++ Librarymanagementsystem.java
@@ -11,7 +11,6 @@
 ✔ Custom Exceptions(BookNotFoundException|MemberNotFoundException|BookAlreadyBorrowedException)
 ✔ Simple Multithreading
 ✔ Admin Login */
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
@@ -23,6 +22,8 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Map.Entry;
+
+import javax.xml.crypto.Data;
 class BookNotFoundException extends Exception{
       @Override
       public String toString() {
@@ -57,7 +58,7 @@ class DataBase implements Serializable{
      this.ISBN = ISBN;
     }
     @Override
-    public String toString() {
+    public String toString(){
         // TODO Auto-generated method stub
         return "Tittle: "+Tittle+", Author: "+author+", Category: "+Category+", ISBN: "+ISBN;
     }
@@ -65,6 +66,7 @@ class DataBase implements Serializable{
 class Library{
     HashMap<Integer,DataBase> dHashMap = new HashMap<>();
     private static final String Path = "Library.ser";
+    Scanner sc = new Scanner(System.in);
     private String Tittle;
     private String author;
     private String Category;
@@ -85,7 +87,6 @@ class Library{
        public void save(){
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(Path))) {
             oos.writeObject(dHashMap);
-            oos.close();
             System.out.println("succesfully saved");
         } catch (Exception e) {
             // TODO: handle exception
@@ -111,13 +112,28 @@ class Library{
              Iterator<Map.Entry<Integer,DataBase>> itr = dHashMap.entrySet().iterator();
              while(itr.hasNext()){
                  Map.Entry<Integer, DataBase> entry = itr.next();
-                 if(someCondition(entry)) itr.remove();
+                 if(someCondition(entry)){
+                    itr.remove();
+                 } 
              }
              save();
        }
        private boolean someCondition(Entry<Integer,DataBase> entry) {
         // Example: delete entries where key > 5
-                return entry.getKey() > 5;
+                return entry.getKey() < 5;
+       }
+       public void SearchBook(){
+        System.out.println("Enter key value: ");
+        int id = sc.nextInt();
+        Iterator<Map.Entry<Integer,DataBase>> itr = dHashMap.entrySet().iterator();
+        while(itr.hasNext()){
+            Map.Entry<Integer,DataBase> entry = itr.next();
+            if(entry.getKey()==id){
+                System.out.println("Key = " + entry.getKey());
+                System.out.println("Value = " + entry.getValue());
+                System.out.println("-------------------");
+            }
+        }
        }
 }
 public class Librarymanagementsystem {
@@ -125,16 +141,45 @@ public class Librarymanagementsystem {
           Library l = new Library();
           Scanner sc = new Scanner(System.in);
         //   l.toAdd("Harry potter", "Kulshingh", "StoryTeller", "123456", 1);
-          l.Display();
-          System.out.println("Type 1 to delete all detail: ");
+        boolean check = true;
+        while(check){
+            System.out.println("Type 1 to add book details: ");
+          System.out.println("Type 2 to delete all detail: ");
+          System.out.println("Type 3 to search book: ");
+          System.out.println("Type 4 to view All details :");
           int num = sc.nextInt();
           switch (num) {
             case 1:
+                System.out.println("Enter id no in integer : ");
+                int id = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Enter a book Tittle Name: ");
+                String tittle = sc.nextLine();
+                System.out.println("Enter a author name: ");
+                String author = sc.nextLine();
+                System.out.println("Enter a book category Name: ");
+                String Category = sc.nextLine();
+                System.out.println("Enter International Standard Book Number: Eg(0-123-45678-9) ");
+                String ISBN = sc.nextLine();
+                String Isbnresult = ISBN.substring(0,1)+"-"+
+                                    ISBN.substring(1, 4)+"-"+
+                                    ISBN.substring(4, 9)+"-"+
+                                    ISBN.substring(9);
+                l.toAdd(tittle, author, Category, Isbnresult, id);
+                break;
+            case 2:
                 l.DeleteAllDetail();
                 break;
-          
+                case 3:
+                    l.SearchBook();
+                    break;
+                    case 4:
+                        l.Display();
+                        break;
             default:
+         check = false;
                 break;
           }
+        }
     } 
 }
