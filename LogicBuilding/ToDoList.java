4ee25942dd7b23209511b3cf6c4d0a04f80db8ad
nirajmgr1010 <neerazmgr9@gmail.com>
2026-04-02 21:56:20 +0545

    Changes

---
+++ LogicBuilding/ToDoList.java
@@ -0,0 +1,27 @@
+/*
+9. To-Do List Application
+Create:
+Add task (task ID, description, status)
+Show all tasks
+Mark task as completed
+Save tasks in file
+*/
+
+import java.io.Serializable;
+
+class Task implements Serializable{
+   private int id;
+   private String name,description;
+    
+}
+class Employee implements Serializable{
+     private int id,age;
+     private String name;
+     Task taskAssigned;
+}
+
+public class ToDoList {
+    public static void main(String[] args) {
+        
+    }
+}
